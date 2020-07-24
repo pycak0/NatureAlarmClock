fe371b0434c24c93a2506b2d5ded4708d7291f56
pycak0 <vladisrusakov@yandex.ru>
2020-07-24 16:51:48 +0300

    fixed small warning

--- NatureAlarmClock/Managers/AlarmsManager.swift
+++ NatureAlarmClock/Managers/AlarmsManager.swift
@@ -38,7 +38,7 @@ class AlarmsManager {
         }
         
         let interval = endDate.timeIntervalSince(startDate)
-        var secondsPerNotification = 30
+        let secondsPerNotification = 30
 //        if interval <= 16 * TimeInterval.secondsIn(.minute) {
 //            secondsPerNotification = 15
 //        }
